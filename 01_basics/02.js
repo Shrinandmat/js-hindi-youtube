f55e7d8da1b18b005f388d3f77ef784f35241d49
Shrinandmat <141657648+Shrinandmat@users.noreply.github.com>
2024-08-08 05:35:19 +0000

    commit

---
+++ 01_basics/02.js
@@ -0,0 +1,72 @@
+console.log("Hello World");
+
+// Operators in JS
+
+// 1. Arithmatic Operators
+
+let a = 4;
+let b = 6;
+
+console.log("a = " , a ,"b = ", b);
+
+console.log("a + b = " , a+b);
+console.log("a - b = " , a-b);
+console.log("a * b = " , a*b);
+console.log("a / b = " , a/b);
+
+
+// Increment and Decrement operators
+// a = 4
+console.log(++a); // a = 5
+console.log(a++); // a = 5
+// a = 6
+
+console.log(--a); // a = 5
+console.log(a--); // a = 5
+// a = 4
+
+// Assignment Operators
+
+a += 5;
+console.log(a); 
+
+b -= 6;
+console.log(b);
+
+a *= 4;
+console.log(a); 
+
+a /= 2;
+console.log(a); 
+
+
+a %= 3;
+console.log(a);
+
+a = 2;
+a **= 2;
+console.log(a);
+
+
+
+// Comparison operator
+
+console.log(5 == 2);
+console.log(5 != 2);
+console.log(5 === 2);
+
+let num1 = 5;
+let num2 = 3;
+
+console.log(num1 == num2);
+console.log(num1 >= num2);
+console.log(num1 === num2);
+console.log(num1 !== num2);
+console.log(num1 != num2);
+console.log(num1 > num2);
+console.log(num1 < num2);
+
+
+
+
+
