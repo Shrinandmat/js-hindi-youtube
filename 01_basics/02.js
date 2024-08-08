7d3740901993730ef6d32cdd3b9dfc2cba62fc93
Shrinandmat <141657648+Shrinandmat@users.noreply.github.com>
2024-08-08 04:29:29 +0000

    commit changes done

---
+++ 01_basics/02.js
@@ -0,0 +1,21 @@
+console.log("Shrinand Mathpati");
+console.log("Welcome to Apna college");
+console.log("My first JS program");
+
+name = "Tony Stark";
+console.log(name);
+
+price = 250;
+console.log(price);
+
+height = 12.5;
+console.log(height);
+
+istrue = false;
+console.log(istrue);
+
+value = null;
+console.log(value);
+
+// ap;
+// console.log(ap);
