04683daaaa87a83d54b760481d375c84c5ae2087
Shrinandmat <141657648+Shrinandmat@users.noreply.github.com>
2024-08-11 05:19:18 +0000

    commit

---
+++ 01_basics/04.js
@@ -0,0 +1,19 @@
+let myyoutubename = "shrinandmathpati";
+
+let anothername = myyoutubename;
+anothername = "chaiaurcode";
+
+console.log(anothername);
+console.log(myyoutubename);
+
+let user1 ={
+    email : "mathpatishreenand@gmail.com",
+    upiId : "xyz@ybl"
+
+}
+
+let user2 = user1;
+
+user2.email = "hitesh@gmail.com";
+console.log(user1.email);
+console.log(user2.email);
