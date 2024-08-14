df8b3d7172e89ec9d75b9e17f5e66e701eb6023f
Shrinandmat <141657648+Shrinandmat@users.noreply.github.com>
2024-08-14 14:36:30 +0000

    commit changes done

---
+++ 03_basics/01.js
@@ -0,0 +1,16 @@
+function Myname() {
+    console.log("S");
+    console.log("H");
+    console.log("R");
+    console.log("I");
+    console.log("N");
+    console.log("A");
+    console.log("N");
+    console.log("D");
+
+}
+
+
+function add(number1 , number2){
+console.log(number1+number2);
+}
