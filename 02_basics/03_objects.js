6dcc2a8c5fe8197cdf4b20da379fce230aef9528
Shrinandmat <141657648+Shrinandmat@users.noreply.github.com>
2024-08-14 12:29:50 +0000

    commit changes done

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,15 @@
+// There are two ways of creating object singleton -> using constructors and object literals
+
+// object literals
+// objects are stored as key value pairs
+const JsUser = {
+ name : "Hitesh",
+ age : 18,
+ location : "Jaipur",
+ email : "hitesh@google.com",
+ isloggedin : false
+}
+
+console.log(JsUser.name);
+
+console.log(JsUser[email]);
