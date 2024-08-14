3bc12497acdb8b61199dbad84f7cd6a1c5f4d520
Shrinandmat <141657648+Shrinandmat@users.noreply.github.com>
2024-08-14 13:04:19 +0000

    cc

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -7,3 +7,15 @@ TinderUser.id = 200;
 TinderUser.name = "Hitesh";
 TinderUser.isloggedin = true;
 console.log(TinderUser);
+
+const regularuser = {
+    email : "someone@gmail.com",
+    fullname : {
+      userfullname :{
+        firstname : "Hitesh",
+        lastname : "Choudhary"
+      }
+    }
+}
+
+console.log(fullname.fullname.firstname);
