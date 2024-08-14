a6e80ab5cfd8c9cabf9856694d5cb0bb044e701c
Shrinandmat <141657648+Shrinandmat@users.noreply.github.com>
2024-08-14 11:31:11 +0000

    commit changes done

---
+++ 01_arrays/01.js
@@ -0,0 +1,10 @@
+const arr = [1,4,5,6,7,9];
+
+console.log(arr[3]);
+
+const heroes = ["Shaktiman" , "Naagraj"];
+console.log(heroes[1]);
+
+const myarr2 = new Array(1,2,3,4,5);
+console.log(myarr2[3]);
+
