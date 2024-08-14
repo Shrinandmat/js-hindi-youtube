6c18494a0f6d29503e910b38b92b61a8921a285f
Shrinandmat <141657648+Shrinandmat@users.noreply.github.com>
2024-08-14 11:35:05 +0000

    commit

--- 01_arrays/01.js
+++ 01_arrays/01.js
@@ -8,3 +8,18 @@ console.log(heroes[1]);
 const myarr2 = new Array(1,2,3,4,5);
 console.log(myarr2[3]);
 
+myarr2.push(6);
+console.log(myarr2[5]);
+
+myarr2.push(7);
+myarr2.pop();
+
+console.log(myarr2);
+myarr2.unshift(9);
+console.log(myarr2);
+
+myarr2.shift(myarr2);
+console.log(myarr2);
+
+
+
