9a176d12f1f24307b34209faafe095473056a2cb
Shrinandmat <141657648+Shrinandmat@users.noreply.github.com>
2024-08-14 12:57:59 +0000

    done

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,9 @@
+// const TinderUser = new Object();
+// This creates singleton object
+
+const TinderUser = {};
+
+TinderUser.id = 200;
+TinderUser.name = "Hitesh";
+TinderUser.isloggedin = true;
+console.log(TinderUser);
