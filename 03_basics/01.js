9ed61aa530cc97d278c256714ed8321417715f61
Shrinandmat <141657648+Shrinandmat@users.noreply.github.com>
2024-08-14 15:36:50 +0000

    commit

--- 03_basics/01.js
+++ 03_basics/01.js
@@ -10,7 +10,34 @@ function Myname() {
 
 }
 
+ 
+// function add(number1 , number2){  // parameters
+// console.log(number1+number2);  // arguments
+// }
 
-function add(number1 , number2){
-console.log(number1+number2);
+function add(number1 , number2){  // parameters
+    // console.log(number1+number2);  // arguments
+    return number1 + number2;
+    }
+
+
+    let result = add(10,20);
+    console.log(result);
+Myname();
+add(3,4);
+
+add(3,"a");
+
+
+function LoginUserMessage(username){
+    if(username === undefined){
+        console.log("Enter a valid username");
+         return
+
+    }
+  return `${username} just logged in`;
 }
+
+
+// console.log(LoginUserMessage("Hitesh"));
+console.log(LoginUserMessage());
