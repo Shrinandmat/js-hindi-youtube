1c56ada20cfe7142c0ef462bd7099018ebb577e2
Shrinandmat <141657648+Shrinandmat@users.noreply.github.com>
2024-08-15 14:51:39 +0000

    commit

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -11,12 +11,29 @@ if(username){
 // truthy value
 // "0" , 'false' , " " , [] , {} , function() {}
 
-if(username.length === 0){
-    console.log("Array is empty");
-}
+// if(username.length === 0){
+//     console.log("Array is empty");
+// }
 
 const emptyobj = {}
 
-if(Object.keys(emptyobj).length == 0){
+if(Object.keys(emptyobj).length === 0){
     console.log("Empty Object");
 }
+
+// Nullish Coalescing operator
+let val1;
+val1 = 5 ?? 10
+console.log(val1);
+
+val1 = null ?? 10;
+console.log(val1);
+
+val1 = undefined ?? 15;
+console.log(val1);
+
+// Ternary operator
+// condition ? true : false
+
+const iceteaprice = 100;
+(iceteaprice <= 80) ? console.log("Less than 80") : console.log("Greater than 80");
