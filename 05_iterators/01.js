56e044158d93215f065fbfaf00f7babd37afc69e
Shrinandmat <141657648+Shrinandmat@users.noreply.github.com>
2024-08-16 13:08:09 +0000

    commit changes done

--- 05_iterators/01.js
+++ 05_iterators/01.js
@@ -2,3 +2,37 @@ for(let index = 0 ; index < 10 ; index++){
     let element = index;
     console.log(element);
 }
+
+for(let i = 0 ; i <= 10 ; i++){
+
+    for(let j = 1 ; j <= 10 ; j++){
+        console.log(`The inner loop value ${i} and inner loop value ${j}`);
+        console.log(i + "*" + j +"="+ i*j);
+    }
+}
+
+let Myarr = ["BatMan" , "Flash" , "Superman"];
+
+for(let i = 0 ; i < Myarr.length ; i++){
+ console.log(Myarr[i]);
+}
+
+for(let index = 1 ; index <= 20 ; index++){
+     if(index == 5){
+        console.log(`5 is detected`);
+        break;
+     }
+console.log(`The value of index is ${index}`);
+
+}
+// Press alt+shift and arrow down key
+for(let index = 1 ; index <= 20 ; index++){
+     if(index == 5){
+        console.log(`5 is detected`);
+        break;
+     }
+console.log(`The value of index is ${index}`);
+
+}
+
+
