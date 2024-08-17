069220a936da640e3a13ac11a08be2aef69550aa
Shrinandmat <141657648+Shrinandmat@users.noreply.github.com>
2024-08-17 06:16:56 +0000

    commit changes done

--- 05_iterators/02.js
+++ 05_iterators/02.js
@@ -3,3 +3,35 @@ while(index <= 10){
     console.log(index);
     index++;
 }
+
+
+const arr = [1,2,3,4,5];
+
+for(let i of arr){
+    console.log(i);
+}
+
+const greeting = "Hello World";
+
+for(const greet of greeting){
+    console.log(`Each char is ${greet}`);
+}
+
+const map = new Map();
+
+map.set('In' , "India");
+map.set('US' , "United States");
+map.set('Fr' , "France");
+
+
+console.log(map);
+
+
+for(const key of map){
+    console.log(key);
+}
+
+
+for(const [key,value] of map){
+    console.log("Key :" + key + " Value : " + value);
+}
